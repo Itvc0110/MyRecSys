355da637952bfbb0c13d74cdcde05b786d7120f3
Itvc0110 <162940999+Itvc0110@users.noreply.github.com>
2025-07-15 16:26:24 +0700

    Update

--- clip/infer.py
+++ clip/infer.py
@@ -107,7 +107,11 @@ if __name__ == "__main__":
     exclude = {'username', 'content_id', 'profile_id'}
     to_convert = [col for col in infer_user_clip_df.columns if col not in exclude]
     infer_user_clip_df[to_convert] = infer_user_clip_df[to_convert].apply(pd.to_numeric, errors='coerce')
+
+    print("Rows before dropna:", len(infer_user_clip_df))
     infer_user_clip_df = infer_user_clip_df.dropna()
+    print("Rows after dropna:", len(infer_user_clip_df))
+    
     infer_user_clip_df = infer_user_clip_df.astype({col: 'float32' for col in infer_user_clip_df.columns if col not in ['username', 'content_id', 'profile_id']})
 
     interaction_df = infer_user_clip_df[['username', 'content_id', 'profile_id']]
