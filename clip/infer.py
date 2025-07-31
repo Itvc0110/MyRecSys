71e14e6a4460a162ce4776963c4777f04708a6f3
Itvc0110 <162940999+Itvc0110@users.noreply.github.com>
2025-08-01 03:01:44 +0700

    user chunk 20

--- clip/infer.py
+++ clip/infer.py
@@ -76,7 +76,7 @@ if __name__ == "__main__":
     part_files = sorted(glob(str(project_root / "clip/infer_data/infer_user_clip/infer_user_clip_part_*.parquet")))
     if not part_files:
         process_infer_data(user_data_path, clip_data_path, num_user=80, num_clip=-1, output_dir_path="clip/infer_data",
-                        user_batch_size=40, chunk_size=None, max_files=-1)
+                        user_batch_size=20, chunk_size=None, max_files=-1)
         part_files = sorted(glob(str(project_root / "clip/infer_data/infer_user_clip/infer_user_clip_part_*.parquet")))
 
     checkpoint_path = "model/clip/best_model.pth"
