5883a6011e803d380b82b815463ba5f1d84da7d0
Itvc0110 <162940999+Itvc0110@users.noreply.github.com>
2025-08-13 09:12:07 +0700

    jbncjncwjcw

--- movie/processing.py
+++ movie/processing.py
@@ -81,7 +81,7 @@ def process_data(output_filepath):
         # label: either watched more than 10% or more than 2 times
         combined_df['label'] = (
             (combined_df['percent_duration'] >= 1) |
-            (combined_df['watch_count'] >= 2)
+            (combined_df['watch_count'] >= 3)
         ).astype(int)
 
         combined_df = combined_df.drop(columns=['percent_duration', 'duration', 'watch_count'], inplace=False)
