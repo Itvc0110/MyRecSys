8d62ece4d3d0f46c30d15b88379d96155aeb7f5a
Itvc0110 <162940999+Itvc0110@users.noreply.github.com>
2025-09-16 14:55:04 +0700

    Harder threshold

--- all/processing.py
+++ all/processing.py
@@ -89,7 +89,7 @@ def process_data(output_filepath):
 
         combined_df['label'] = (
             #(combined_df['percent_duration'] >= 0.3) |
-            (combined_df['watch_count'] >= 2)
+            (combined_df['watch_count'] >= 3)
         ).astype(int)
 
         combined_df = combined_df.drop(columns=['duration', 'watch_count'], inplace=False)
